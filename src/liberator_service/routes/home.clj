4aa4c5bc8947f0e59cdd8e61ae98765561fd4b99
Jonathan Headen <jonathan.headen@outlook.com>
2016-01-29 07:21:01 +1100

    testing :handle-service-not-available

--- src/liberator_service/routes/home.clj
+++ src/liberator_service/routes/home.clj
@@ -4,14 +4,17 @@
              :refer [defresource resource request-method-in]]))
 
 (defresource home
-  :service-available? true
+  :service-available? false
+  :handle-service-not-available
+  "This service is currently unavailable..."
+
   :method-allowed? (request-method-in :get)
   :handle-method-not-allowed
   (fn [context]
     (str (get-in context [:request :request-method]) " is not allowed"))
+
   :handle-ok "Hello World!"
-  :etag "fixed-etag"
-  :available-media-types ["text/plain"])
+  :etag "fixed-etag" :available-media-types ["text/plain"])
 
 (defroutes home-routes
   (ANY "/" request home))
