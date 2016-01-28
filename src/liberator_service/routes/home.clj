1bda1c55ba0c613d7f4bacfae01b743555c18258
Jonathan Headen <jonathan.headen@outlook.com>
2016-01-28 17:57:00 +1100

    restricted allowable services to GET

--- src/liberator_service/routes/home.clj
+++ src/liberator_service/routes/home.clj
@@ -4,7 +4,7 @@
             ))
 
 (defresource home
-  :service-available? false
+  :allowed-methods [:get]
   :handle-ok "Hello World!"
   :etag "fixed-etag"
   :available-media-types ["text/plain"])
